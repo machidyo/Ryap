ff19401a724016ea55755b3445bf21852781da38
machidyo <yorihiko_m@hotmail.com>
2021-03-14 15:34:03 +0900

    modify: loop を使わずに threading する方式に変更

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,16 @@
 #define TASK_DEFAULT_CORE_ID 1
 #define TASK_STACK_DEPTH 4096UL
 #define TASK_NAME_IMU "IMUTask"
-#define TASK_SLEEP_IMU 5  // = 1000[ms] / 200[Hz]
+#define TASK_NAME_WRITE_SESSION "WriteSessionTask"
+#define TASK_SLEEP_IMU 5            // = 1000[ms] / 200[Hz]
+#define TASK_SLEEP_WRITE_SESSION 40 // = 1000[ms] / 25[Hz]
 #define MUTEX_DEFAULT_WAIT 1000UL
 
+void initM5LCD();
+void initGyro();
+void initWifi();
 static void ImuLoop(void *arg);
+static void WriteSessionLoop(void* arg);
 
 imu::ImuReader *imuReader;
 imu::ImuData imuData;
@@ -32,14 +38,38 @@ bool gyroOffsetInstalled = true;
 imu::AverageCalcXYZ gyroAve;
 prefs::Settings settingPref;
 
-void InitM5LCD() {
+void setup() {
+    M5.begin();
+    Serial.begin(115200);
+
+    initM5LCD();
+
+    M5.Lcd.print("-- ryap --");
+
+    initGyro();
+    initWifi();
+
+    M5.Lcd.println(WiFi.localIP());
+
+    imuDataMutex = xSemaphoreCreateMutex();
+    xTaskCreatePinnedToCore(ImuLoop, TASK_NAME_IMU, TASK_STACK_DEPTH, NULL, 
+        2, NULL, TASK_DEFAULT_CORE_ID);
+    xTaskCreatePinnedToCore(WriteSessionLoop, TASK_NAME_WRITE_SESSION, TASK_STACK_DEPTH, NULL, 
+        1, NULL, TASK_DEFAULT_CORE_ID);
+}
+
+void loop() { 
+    // nothing to do
+}
+
+void initM5LCD() {
     M5.Lcd.fillScreen(BLACK);
     M5.Lcd.setRotation(3);
     M5.Lcd.setTextSize(1);
     M5.Lcd.setCursor(2, 0);
 }
 
-void InitGyro() {
+void initGyro() {
     float gyroOffset[3] = {0.0F};
     settingPref.begin();
     settingPref.readGyroOffset(gyroOffset);
@@ -52,7 +82,7 @@ void InitGyro() {
     }
 }
 
-void InitWifi() {
+void initWifi() {
     M5.Mpu6886.Init();
     Serial.println("[ESP32] Connecting to WiFi network: " + String(SSID));
     WiFi.disconnect(true, true);
@@ -65,42 +95,6 @@ void InitWifi() {
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
-}
-
-void setup() {
-    M5.begin();
-    Serial.begin(115200);
-
-    InitM5LCD();
-
-    M5.Lcd.print("-- ryap --");
-
-    InitGyro();
-    InitWifi();
-
-    M5.Lcd.println(WiFi.localIP());
-
-    imuDataMutex = xSemaphoreCreateMutex();
-    xTaskCreatePinnedToCore(ImuLoop, TASK_NAME_IMU, TASK_STACK_DEPTH, NULL, 2,
-                            NULL, TASK_DEFAULT_CORE_ID);
-}
-
-void sendUDP() {
-    udp.beginPacket(CLIENT_ADDRESS, CLIENT_PORT);
-    for (int i = 0; i < SEND_DATA_NUM; i++) {
-        byte data[sizeof(float)];
-        memcpy(data, &imuData.quat[i], sizeof(imuData.quat[i]));
-        udp.write(data, sizeof(data));
-    }
-    udp.endPacket();
-}
-
-void loop() {
-    if (gyroOffsetInstalled) {
-        sendUDP();
-    }
-
-    delay(5);
 }
 
 static void ImuLoop(void *arg) {
@@ -132,4 +126,25 @@ static void ImuLoop(void *arg) {
         int32_t sleep = TASK_SLEEP_IMU - (millis() - entryTime);
         vTaskDelay((sleep > 0) ? sleep : 0);
     }
+}
+
+static void WriteSessionLoop(void* arg) {
+  while (1) {
+    uint32_t entryTime = millis();
+    if (gyroOffsetInstalled) {
+      if (xSemaphoreTake(imuDataMutex, MUTEX_DEFAULT_WAIT) == pdTRUE) {
+            udp.beginPacket(CLIENT_ADDRESS, CLIENT_PORT);
+            for (int i = 0; i < SEND_DATA_NUM; i++) {
+                byte data[sizeof(float)];
+                memcpy(data, &imuData.quat[i], sizeof(imuData.quat[i]));
+                udp.write(data, sizeof(data));
+            }
+            udp.endPacket();
+      }
+      xSemaphoreGive(imuDataMutex);
+    }
+    // idle
+    int32_t sleep = TASK_SLEEP_WRITE_SESSION - (millis() - entryTime);
+    vTaskDelay((sleep > 0) ? sleep : 0);
+  }
 }
